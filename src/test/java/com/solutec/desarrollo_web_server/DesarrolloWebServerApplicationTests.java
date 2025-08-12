c88e7b5a674d1a67fde1c6faf5105b40f0c78123
wilmer <wsinay@bytesw.com>
2025-08-11 20:53:52 -0600

    Primer commit

---
+++ src/test/java/com/solutec/desarrollo_web_server/DesarrolloWebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solutec.desarrollo_web_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesarrolloWebServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
