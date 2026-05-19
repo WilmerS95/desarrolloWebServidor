0e47ddb4765eb0d8131be30ed7ed98af11263c86
wilmer <wsinay@bytesw.com>
2026-05-19 08:24:36 -0600

    Se inicia la base para sistema campeonatos deportivos

--- loan-application-service/src/main/java/com/solutec/loan_application_server/repository/CategoryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.solutec.loan_application_server.repository;
-
-import com.solutec.loan_application_server.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> { }
--- loan-application-service/src/main/java/com/solutec/loan_application_server/repository/ItemRepository.java
+++
@@ -1,6 +0,0 @@
-package com.solutec.loan_application_server.repository;
-
-import com.solutec.loan_application_server.entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {}
